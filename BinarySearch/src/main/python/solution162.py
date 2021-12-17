c98428b48bdeeec922ac57267a23c173e6a0df34
azheng <azhengbot@outlook.com>
2021-12-17 20:04:56 +0800

    162. 寻找峰值

--- BinarySearch/src/main/python/solution162.py
+++ BinarySearch/src/main/python/solution162.py
@@ -3,4 +3,16 @@ from typing import List
 
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        ...
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            left_mid = (left + right) // 2
+            right_mid = left_mid + 1
+
+            if nums[left_mid] >= nums[right_mid]:
+                right = left_mid
+
+            else:
+                left = left_mid + 1
+
+        return right
