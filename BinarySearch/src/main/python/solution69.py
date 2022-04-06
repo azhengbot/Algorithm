adf9cd8e256b6f3e3f0d53814f2964c457cd5d98
azhengbot <azhengbot@outlook.com>
2022-04-06 11:00:30 +0800

    add: binary search

--- BinarySearch/src/main/python/solution69.py
+++ BinarySearch/src/main/python/solution69.py
@@ -1,13 +1,53 @@
+# SWORD_OFFER
+# @lc app=leetcode.cn id=69 lang=python3
+#
+# [69] Sqrt(x)
+#
+# https://leetcode-cn.com/problems/sqrtx/description/
+#
+# algorithms
+# Easy (39.09%)
+# Likes:    841
+# Dislikes: 0
+# Total Accepted:    424.7K
+# Total Submissions: 1.1M
+# Testcase Example:  '4'
+#
+# 给你一个非负整数 x ，计算并返回 x 的 算术平方根 。
+# 
+# 由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 。
+# 
+# 注意：不允许使用任何内置指数函数和算符，例如 pow(x, 0.5) 或者 x ** 0.5 。
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：x = 4
+# 输出：2
+# 
+# 
+# 示例 2：
+# 
+# 
+# 输入：x = 8
+# 输出：2
+# 解释：8 的算术平方根是 2.82842..., 由于返回类型是整数，小数部分将被舍去。
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 0 <= x <= 2^31 - 1
+# 
+# 
+#
+
+# @lc code=start
 class Solution:
     def mySqrt(self, x: int) -> int:
-        left, right = 0, x
+# @lc code=end
 
-        while left < right:
-            mid = (left + right + 1) // 2
-
-            if mid * mid <= x:
-                left = mid
-            else:
-                right = mid - 1
-
-        return right
