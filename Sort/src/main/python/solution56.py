d643b205b6c4fbb01d5f3fad5b1effb71d4de248
azhengbot <azhengbot@outlook.com>
2022-04-07 11:32:51 +0800

    add: sort

--- Sort/src/main/python/solution56.py
+++ Sort/src/main/python/solution56.py
@@ -1,34 +1,52 @@
-from typing import List
+# SWORD_OFFER
+# @lc app=leetcode.cn id=56 lang=python3
+#
+# [56] 合并区间
+#
+# https://leetcode-cn.com/problems/merge-intervals/description/
+#
+# algorithms
+# Medium (48.05%)
+# Likes:    1417
+# Dislikes: 0
+# Total Accepted:    413.8K
+# Total Submissions: 856.2K
+# Testcase Example:  '[[1,3],[2,6],[8,10],[15,18]]'
+#
+# 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi]
+# 。请你合并所有重叠的区间，并返回 一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间 。
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+# 输出：[[1,6],[8,10],[15,18]]
+# 解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+# 
+# 
+# 示例 2：
+# 
+# 
+# 输入：intervals = [[1,4],[4,5]]
+# 输出：[[1,5]]
+# 解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 1 <= intervals.length <= 10^4
+# intervals[i].length == 2
+# 0 <= starti <= endi <= 10^4
+# 
+# 
+#
 
-
+# @lc code=start
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals = sorted(intervals, key=lambda x: x[0])
+# @lc code=end
 
-        n = len(intervals)
-
-        ans = []
-
-        start_0, start_1 = intervals[0]
-
-        i = 0
-        while i < n:
-            if intervals[i][0] < start_1:
-                if intervals[i][1] > start_1:
-                    start_1 = intervals[i][1]
-                i += 1
-
-            elif intervals[i][0] > start_1:
-                ans.append([start_0, start_1])
-
-                start_0, start_1 = intervals[i]
-
-                i += 1
-
-            elif intervals[i][0] == start_1:
-                start_1 = intervals[i][1]
-                i += 1
-
-        ans.append([start_0, start_1])
-
-        return ans
