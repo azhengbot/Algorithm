4a94ef107dc1b0021d5fae2189ea2d312fbc6f10
xuhaoqing <xuhaoqing@lattebank.com>
2024-02-20 14:43:31 +0800

    [105] 从前序与中序遍历序列构造二叉树

---
+++ Tree/src/main/java/105.从前序与中序遍历序列构造二叉树.java
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode.cn id=105 lang=java
+ *
+ * [105] 从前序与中序遍历序列构造二叉树
+ *
+ * https://leetcode.cn/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
+ *
+ * algorithms
+ * Medium (71.17%)
+ * Likes:    2229
+ * Dislikes: 0
+ * Total Accepted:    589.3K
+ * Total Submissions: 825.7K
+ * Testcase Example:  '[3,9,20,15,7]\n[9,3,15,20,7]'
+ *
+ * 给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历， inorder
+ * 是同一棵树的中序遍历，请构造二叉树并返回其根节点。
+ * 
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+ * 输出: [3,9,20,null,null,15,7]
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: preorder = [-1], inorder = [-1]
+ * 输出: [-1]
+ * 
+ * 
+ * 
+ * 
+ * 提示:
+ * 
+ * 
+ * 1 <= preorder.length <= 3000
+ * inorder.length == preorder.length
+ * -3000 <= preorder[i], inorder[i] <= 3000
+ * preorder 和 inorder 均 无重复 元素
+ * inorder 均出现在 preorder
+ * preorder 保证 为二叉树的前序遍历序列
+ * inorder 保证 为二叉树的中序遍历序列
+ * 
+ * 
+ */
+
+// @lc code=start
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        List<Integer> inorderList = Arrays.stream(inorder)
+                .boxed()
+                .collect(Collectors.toList());
+        if (preorder.length == 0) {
+            return null;
+        }
+
+        int idx = inorderList.indexOf(preorder[0]);
+        TreeNode root = new TreeNode(preorder[0]);
+
+        root.left = buildTree(Arrays.copyOfRange(preorder, 1, idx + 1), Arrays.copyOfRange(inorder, 0, idx + 1));
+        root.right = buildTree(Arrays.copyOfRange(preorder, idx + 1, preorder.length),
+                Arrays.copyOfRange(inorder, idx + 1, inorder.length));
+
+        return root;
+    }
+}
+// @lc code=end
