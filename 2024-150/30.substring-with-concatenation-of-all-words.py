2800aaf972bff4a23b1f7e3fcd63c622085aa437
xuhaoqing <xuhaoqing@lattebank.com>
2024-11-19 14:55:41 +0800

    [45] 跳跃游戏 II

--- 2024-150/30.substring-with-concatenation-of-all-words.py
+++ 2024-150/30.substring-with-concatenation-of-all-words.py
@@ -1,4 +1,4 @@
-#
+# STUCK
 # @lc app=leetcode.cn id=30 lang=python3
 # @lcpr version=30204
 #
---
+++ 2024-150/42.trapping-rain-water.py
@@ -0,0 +1,72 @@
+# STUCK
+# @lc app=leetcode.cn id=42 lang=python3
+# @lcpr version=30204
+#
+# [42] 接雨水
+#
+# https://leetcode.cn/problems/trapping-rain-water/description/
+#
+# algorithms
+# Hard (64.36%)
+# Likes:    5390
+# Dislikes: 0
+# Total Accepted:    1.1M
+# Total Submissions: 1.7M
+# Testcase Example:  '[0,1,0,2,1,0,1,3,2,1,2,1]'
+#
+# 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 
+# 输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+# 输出：6
+# 解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。 
+# 
+# 
+# 示例 2：
+# 
+# 输入：height = [4,2,0,3,2,5]
+# 输出：9
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# n == height.length
+# 1 <= n <= 2 * 10^4
+# 0 <= height[i] <= 10^5
+# 
+# 
+#
+
+
+# @lcpr-template-start
+
+# @lcpr-template-end
+# @lc code=start
+from typing import List
+class Solution:
+    def trap(self, height: List[int]) -> int:
+
+        
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# [0,1,0,2,1,0,1,3,2,1,2,1]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [4,2,0,3,2,5]\n
+# @lcpr case=end
+
+#
+
