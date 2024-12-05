0cbeecd86d13372c7fab60e3cbd411aebd0cd7fa
azheng <azhengbot@outlook.com>
2024-12-05 20:32:57 +0800

    [148] 排序链表

--- 2024-150/148.sort-list.py
+++ 2024-150/148.sort-list.py
@@ -15,56 +15,99 @@
 # Testcase Example:  '[4,2,1,3]'
 #
 # 给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
-# 
-# 
-# 
-# 
-# 
-# 
+#
+#
+#
+#
+#
+#
 # 示例 1：
-# 
+#
 # 输入：head = [4,2,1,3]
 # 输出：[1,2,3,4]
-# 
-# 
+#
+#
 # 示例 2：
-# 
+#
 # 输入：head = [-1,5,3,4,0]
 # 输出：[-1,0,3,4,5]
-# 
-# 
+#
+#
 # 示例 3：
-# 
+#
 # 输入：head = []
 # 输出：[]
-# 
-# 
-# 
-# 
+#
+#
+#
+#
 # 提示：
-# 
-# 
+#
+#
 # 链表中节点的数目在范围 [0, 5 * 10^4] 内
 # -10^5 <= Node.val <= 10^5
-# 
-# 
-# 
-# 
+#
+#
+#
+#
 # 进阶：你可以在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？
-# 
+#
 #
 
+
+# @lcpr-template-start
+
+# @lcpr-template-end
 # @lc code=start
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
-# @lc code=end
+from typing import Optional
 
+
+class Solution:
+    def get_head2(self, head):
+        slow = head
+        fast = head
+        while slow.next and fast.next and fast.next.next:
+            # print(slow)
+            slow = slow.next
+            fast = fast.next.next
+        mid = slow.next
+        slow.next = None
+        return mid
+
+    def head_merge(self, head, head2):
+        dummy = ListNode()
+        curr = dummy
+        while head and head2:
+            if head.val < head2.val:
+                curr.next = head
+                head = head.next
+            else:
+                curr.next = head2
+                head2 = head2.next
+            curr = curr.next
+        if head:
+            curr.next = head
+        if head2:
+            curr.next = head2
+        return dummy.next
+
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            return head
+        head2 = self.get_head2(head)
+        # print(head2)
+        head = self.sortList(head)
+        head2 = self.sortList(head2)
+
+        return self.head_merge(head, head2)
+
+
+# @lc code=end
 
 
 #
@@ -81,4 +124,3 @@ class Solution:
 # @lcpr case=end
 
 #
-
