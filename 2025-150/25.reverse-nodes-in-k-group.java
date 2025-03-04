22448fada356561b47a798e8b45b09e83376ffdc
xuhaoqing <xuhaoqing@lattebank.com>
2025-03-04 16:09:04 +0800

    [76] 最小覆盖子串

---
+++ 2025-150/25.reverse-nodes-in-k-group.java
@@ -0,0 +1,119 @@
+/*
+ * @lc app=leetcode.cn id=25 lang=java
+ * @lcpr version=30204
+ *
+ * [25] K 个一组翻转链表
+ *
+ * https://leetcode.cn/problems/reverse-nodes-in-k-group/description/
+ *
+ * algorithms
+ * Hard (68.68%)
+ * Likes:    2430
+ * Dislikes: 0
+ * Total Accepted:    692.5K
+ * Total Submissions: 1M
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * 给你链表的头节点 head ，每 k 个节点一组进行翻转，请你返回修改后的链表。
+ * 
+ * k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+ * 
+ * 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：head = [1,2,3,4,5], k = 2
+ * 输出：[2,1,4,3,5]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 
+ * 输入：head = [1,2,3,4,5], k = 3
+ * 输出：[3,2,1,4,5]
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 链表中的节点数目为 n
+ * 1 <= k <= n <= 5000
+ * 0 <= Node.val <= 1000
+ * 
+ * 
+ * 
+ * 
+ * 进阶：你可以设计一个只用 O(1) 额外内存空间的算法解决此问题吗？
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+// @lcpr-template-start
+
+// @lcpr-template-end
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        int n = 0;
+        ListNode curr = head;
+        while (curr != null) {
+            n++;
+            curr = curr.next;
+        }
+        ListNode dummy = new ListNode();
+        dummy.next = head;
+        ListNode p = dummy;
+        
+        while (n >= k) {
+            n = n - k;
+            curr = p.next;
+            ListNode pre = null;
+            for (int i=0; i<k; i++) {
+                System.out.println(curr.val);
+                ListNode nxt = curr.next;
+                curr.next = pre;
+                pre = curr;
+                curr = nxt;
+            }
+            ListNode nextP = p.next;
+            p.next.next = curr;
+            p.next = pre;
+            p = nextP;
+            
+        }
+
+        return dummy.next;
+    }
+}
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,2,3,4,5]\n2\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,2,3,4,5]\n3\n
+// @lcpr case=end
+
+ */
+
